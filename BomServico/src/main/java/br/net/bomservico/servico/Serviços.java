1c05373821b9d6f5ffc4631c9e03ac330f5253ff
wesleygomes <falecomweslley@gmail.com>
2014-10-02 15:29:33 -0300

    Commit Serviços

--- BomServico/src/main/java/br/net/bomservico/servico/Serviços.java
+++ BomServico/src/main/java/br/net/bomservico/servico/Serviços.java
@@ -1,7 +1,53 @@
 package br.net.bomservico.servico;
 
 public class Serviços {
+
+	// Cadastrar o Usuario
+	public String Logar(String login, String senha) {
+		return "";
+	}
+
+	// Cadastrar o Usuario
+	public String CadastrarUsuario(String nome, String sobrenome, String login, String senha) {
+		return "";
+	}
+
+	// Cadastrar o Serviço
+	public String CadastrarServico(String nome, String sobrenome, String email,
+			String sexo, String login, String senha, int cpf,
+			String foto_perfil, int celular, int telefone_fixo) {
+		
+		return "";
+	}
+
+	// Alterar o Usuario
+	public void AlterarServico(int numA, int numB) {
+
+	}
+
+	// Excluir o Usuario
+	public void ExcluirServico(int numA, int numB) {
+
+	}
+
+	// Pesquisar o Serviço
+	public void PesquisarServico(int numA, int numB) {
+
+	}
+
+	// Enviar Email para usuario
+	public void EnviarEmail(int numA, int numB) {
+
+	}
+
+	// Exibir o perfil do usuario
+	public void perfilUsuario(int numA, int numB) {
+
+	}
 	
+	public void deslogar(){
+		
+	}
 	
 
 }
